5752821901dd90648254d41a010d2dabec560f90
Ilya-Lisov999 <157629799+Ilya-Lisov999@users.noreply.github.com>
2024-02-05 18:42:26 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+// let name = "Іван";
+//Let city = "Kyiv";
+//let city = name;
+//console.log = (city);
+
+//let name = "Ilya";
+//console.log('привіт ${1}'); // ?
+//console.log('привіт ${name}'); // ?
+//console.log('привіт ${name}'); // ?
+
+let a = "7"
+let b = "78gth"
+let c = "89.23drtc"
+console.log(typeof Number(a))
+console.log(typeof parseInt(b))
+console.log(typeof parseFloat(c))
+
+console.log((0.1 * 10 + 0.2 * 10) / 10)
+
+console.log(Math.max(20, 10, 40, 50))
+
+console.log(Math.random()*(4-2) +2 )
